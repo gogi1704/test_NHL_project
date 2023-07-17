980a492401d478a97c8854f65fac57b12bd43a47
George <96864675+gogi1704@users.noreply.github.com>
2023-07-18 00:22:29 +0300

    create LiveFragment

--- app/src/main/java/com/example/test_nhl_project/ui/fragments/LiveGameFragment.kt
+++ app/src/main/java/com/example/test_nhl_project/ui/fragments/LiveGameFragment.kt
@@ -1,6 +1,7 @@
 package com.example.test_nhl_project.ui.fragments
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,18 +11,95 @@ import com.example.test_nhl_project.databinding.FragmentLiveGameBinding
 
 class LiveGameFragment : Fragment() {
     private lateinit var binding: FragmentLiveGameBinding
+    private lateinit var countDownTimer: CountDownTimer
+    private var isTimerRunning = false
+    private var remainingTimeInMillis: Long = 0
+    private val timerDurationInMillis: Long = 3600_000 // Продолжительность таймера в миллисекундах
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-       binding = FragmentLiveGameBinding.inflate(layoutInflater , container , false)
+        binding = FragmentLiveGameBinding.inflate(layoutInflater, container, false)
+
+        countDownTimer = object : CountDownTimer(timerDurationInMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                remainingTimeInMillis = millisUntilFinished
+                binding.textMin.text = (remainingTimeInMillis / 1000 / 60).toString()
+                binding.textSec.text = (remainingTimeInMillis / 1000 % 60).toString()
+                updateUI()
+            }
+
+            override fun onFinish() {
+                isTimerRunning = false
+                // Действия после завершения таймера
+                // Например, выполнение определенных действий или отображение сообщения
+            }
+        }
+
+        with(binding) {
+            buttonFirstPlus.setOnClickListener {
+                val num = textFirstCounter.text.toString().toInt() + 1
+                textFirstCounter.text = num.toString()
+                toggleButtonFirst.clearChecked()
+            }
+            buttonFirstMinus.setOnClickListener {
+                val num = textFirstCounter.text.toString().toInt() - 1
+                textFirstCounter.text = num.toString()
+                toggleButtonFirst.clearChecked()
+            }
+
+            buttonSecondPlus.setOnClickListener {
+                val num = textSecondCounter.text.toString().toInt() + 1
+                textSecondCounter.text = num.toString()
+                toggleButtonSecond.clearChecked()
+            }
+            buttonSecondMinus.setOnClickListener {
+                val num = textSecondCounter.text.toString().toInt() - 1
+                textSecondCounter.text = num.toString()
+                toggleButtonSecond.clearChecked()
+            }
+
+            buttonStart.setOnClickListener {
+                startTimer()
+                toggleButtonStart.clearChecked()
+            }
+
+            buttonReset.setOnClickListener {
+                resetTimer()
+                textMin.text = "60"
+                textSec.text = "00"
+                toggleButtonStart.clearChecked()
+            }
 
 
+        }
 
 
 
         return binding.root
+    }
+
+
+    private fun startTimer() {
+        countDownTimer.start()
+        isTimerRunning = true
+    }
+
+    private fun pauseTimer() {
+        countDownTimer.cancel()
+        isTimerRunning = false
+    }
+
+    private fun resetTimer() {
+        countDownTimer.cancel()
+        isTimerRunning = false
+        remainingTimeInMillis = timerDurationInMillis
+        updateUI()
+    }
+
+    private fun updateUI() {
+        // Обновите ваш интерфейс (например, TextView) для отображения оставшегося времени
     }
 
 
